0a8dfcb3955c38dc3d055dceb57f220c01cd51de
masterthuku <peterthuku706@gmail.com>
2024-02-23 10:53:52 +0300

    solve

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
  
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
  
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  //ignoredRoutes: ['/no-auth-in-this-route'],
 });
  
 export const config = {
